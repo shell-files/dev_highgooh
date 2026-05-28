9f4f72a8fdc717f95455d8eb3cea0a53193abc51
choiyc05 <choiyc05@gmail.com>
2026-05-28 16:05:27 +0900

    frontend init

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router";
+import App from '@/homes/App.jsx'
+import { AuthProvider } from '@hooks/AuthContext.jsx'
+import { Provider } from 'react-redux'
+import store from '@stores'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+				<AuthProvider>
+					<App />
+				</AuthProvider>
+			</Provider>
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ frontend/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    allowedHosts: ['localhost', 'weareithero.cloud', 'myapp.com'],
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@errors': fileURLToPath(new URL('./src/homes/errors', import.meta.url)),
+      '@gates': fileURLToPath(new URL('./src/homes/gates', import.meta.url)),
+      '@logins': fileURLToPath(new URL('./src/homes/logins', import.meta.url)),
+      '@mains': fileURLToPath(new URL('./src/homes/mains', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+    }
+  },
+})
