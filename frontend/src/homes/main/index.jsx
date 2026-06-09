cc78b61e9307add7fbe2085f5d9af1fb772fc8a8
choegayoung <chlrkdud5642@gmail.com>
2026-06-09 17:52:59 +0900

    feature/Anomaly페이지 리액트화(#21)

--- frontend/src/homes/main/index.jsx
+++ frontend/src/homes/main/index.jsx
@@ -4,6 +4,7 @@ import Sidebar from '@components/Layout/Sidebar.jsx';
 import Main from '@homes/Main/Main.jsx';
 import Asn from '@homes/inbound/Asn.jsx';
 import '@styles/common.css';
+import Anomaly from '@homes/anomaly/Anomaly.jsx';
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
             <Routes>
               <Route path='/' element={<Main />}/>
               <Route path='/asn' element={<Asn />}/>
+              <Route path='/anomaly' element={<Anomaly />}/>
             </Routes>
           </div>
       </div>
