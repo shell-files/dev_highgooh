28e5714e0de4d2733b13a987005d6d23a108a9cc
mpsddd-commits <mpsddd@gmail.com>
2026-06-10 20:24:03 +0900

    css 수정 및 구현 중

--- frontend/src/homes/outbound/Outbound.jsx
+++ frontend/src/homes/outbound/Outbound.jsx
@@ -1,11 +1,11 @@
 import '@styles/outbound.css';
 import { useState } from 'react';
 
-
 const Outbound = () => {
 
+
     return (
-        <>
+        <div id="outbound-page">
             <div className="page-header-flex">
                 <h2 className="page-title">출고/송장</h2>
                 <div className="header-action-group">
@@ -69,14 +69,6 @@ const Outbound = () => {
 
             <div className="filter-wrapper-card">
                 <form className="search-filter-grid">
-                    <div className="filter-group group-date-range">
-                        <label>주문 기간</label>
-                        <div className="date-range-container">
-                            <input type="date" id="search_start_date" className="filter-control" />
-                            <span className="date-separator">~</span>
-                            <input type="date" id="search_end_date" className="filter-control" />
-                        </div>
-                    </div>
                     <div className="filter-group">
                         <label>주문번호 검색</label>
                         <div className="date-range-container">
@@ -107,8 +99,73 @@ const Outbound = () => {
                     </div>
                 </form>
             </div>
-                    
-        </>
+            <div className="content-card">
+                <div className="table-responsive">
+                    <div className="inhistory-btn-group">
+                        <button type="button" className="btn-filter-reset">엑셀 다운로드</button>
+                        <button type="submit" className="btn-filter-search">새로고침</button>
+                    </div>
+                    <table className="history-data-table">
+                        <thead>
+                            <tr>
+                                <th>출고일자</th>
+                                <th>매니페스트 번호</th>
+                                <th>운송사</th>
+                                <th>출고 제품 수량(세트)</th>
+                                <th>요청 수량</th>
+                            </tr>
+                        </thead>
+                        <tbody id="historyTableBody">
+                            <tr>
+                                <td className="text-center">2026-05-28</td>
+                                <td className="font-bold text-link">
+                                    ASN-20260602-001</td>
+                                <td className="text-center">대한택배</td>
+                                <td>15</td>
+                                <td className="text-center">5,000</td>
+                            </tr>
+                            <tr>
+                                <td className="text-center">2026-05-26</td>
+                                <td className="font-bold text-link">
+                                    ASN-20260602-002</td>
+                                <td className="text-center">대한택배</td>
+                                <td>15</td>
+                                <td className="text-center">2,500</td>
+                            </tr>
+                            <tr>
+                                <td className="text-center">2026-05-26</td>
+                                <td className="font-bold text-link">
+                                    ASN-20260602-002</td>
+                                <td className="text-center">대한택배</td>
+                                <td>15</td>
+                                <td className="text-center">2,500</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="pagination-container">
+                    <div className="pagination-info">
+                        전체 <span>3</span>건
+                    </div>
+
+                    <div className="pagination-buttons">
+                        <button type="button" className="btn-page" title="처음 페이지" disabled>&lt;&lt;</button>
+                        <button type="button" className="btn-page" title="이전 블록" disabled>&lt;</button>
+
+                        <button type="button" className="btn-page-num active">1</button>
+                        <button type="button" className="btn-page-num">2</button>
+                        <button type="button" className="btn-page-num">3</button>
+                        <button type="button" className="btn-page-num">4</button>
+                        <button type="button" className="btn-page-num">5</button>
+
+                        <button type="button" className="btn-page" title="다음 블록">&gt;</button>
+                        <button type="button" className="btn-page" title="끝 페이지">&gt;&gt;</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
