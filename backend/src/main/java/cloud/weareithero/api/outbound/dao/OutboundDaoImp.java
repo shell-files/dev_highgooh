6e7bffd2908e2eaa1131eb49866158da6c50f663
frankie516 <frankie516c@gmail.com>
2026-06-11 16:32:29 +0900

    Mapper 수정에 따른 연쇄 코드 수정 (#25)

--- backend/src/main/java/cloud/weareithero/api/outbound/dao/OutboundDaoImp.java
+++ backend/src/main/java/cloud/weareithero/api/outbound/dao/OutboundDaoImp.java
@@ -4,20 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import cloud.weareithero.api.outbound.dto.OutboundCarrierDTO;
 import cloud.weareithero.api.outbound.dto.OutboundDTO;
-import cloud.weareithero.api.outbound.dto.OutboundInvoiceDTO;
-import cloud.weareithero.api.outbound.dto.OutboundManifestDTO;
-import cloud.weareithero.api.outbound.dto.OutboundPackingDTO;
 import cloud.weareithero.api.outbound.dto.OutboundProductDTO;
 import cloud.weareithero.api.outbound.dto.OutboundRequestDTO;
-import cloud.weareithero.api.outbound.dto.OutboundSummaryDTO;
-import cloud.weareithero.api.outbound.dto.OutboundTransportationVehicleDTO;
-import cloud.weareithero.api.outbound.dto.OutboundVehicleAssignDTO;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class OutboundDaoImp implements OutboundDao {
@@ -25,65 +16,22 @@ public class OutboundDaoImp implements OutboundDao {
     private final OutboundMapper outboundMapper;
 
     @Override
-    public OutboundSummaryDTO findSummary(OutboundRequestDTO outboundRequestDTO) {
-        return outboundMapper.findSummary(outboundRequestDTO);
-    }
-
-    @Override
-    public List<OutboundPackingDTO> findAll(OutboundRequestDTO outboundRequestDTO) {
+    public List<OutboundDTO> findAll(OutboundRequestDTO outboundRequestDTO) {
         return outboundMapper.findAll(outboundRequestDTO);
     }
 
     @Override
-    public OutboundDTO findByOutboundId(int outboundId) {
-        return outboundMapper.findByOutboundId(outboundId);
+    public int countAll(OutboundRequestDTO outboundRequestDTO) {
+        return outboundMapper.countAll(outboundRequestDTO);
     }
 
     @Override
-    public OutboundPackingDTO findByPackingId(int packingId) {
-        return outboundMapper.findByPackingId(packingId);
+    public List<OutboundProductDTO> findOne(int outboundId) {
+        return outboundMapper.findOne(outboundId);
     }
 
     @Override
-    public List<OutboundProductDTO> findProducts(int outboundId) {
-        return outboundMapper.findProducts(outboundId);
+    public OutboundDTO findbyOutboundId(int outboundId) {
+        return outboundMapper.findbyOutboundId(outboundId);
     }
-
-    @Override
-    public List<OutboundManifestDTO> findAllManifest(OutboundRequestDTO outboundRequestDTO) {
-        return outboundMapper.findAllManifest(outboundRequestDTO);
-    }
-
-    @Override
-    public List<OutboundCarrierDTO> findByCarrier() {
-        return outboundMapper.findByCarrier();
-    }
-
-    @Override
-    public List<OutboundTransportationVehicleDTO> findByVehicle() {
-        return outboundMapper.findByVehicle();
-    }
-
-    @Override
-    public int addTransportation(OutboundVehicleAssignDTO outboundVehicleAssignDTO) {
-        outboundMapper.addTransportation(outboundVehicleAssignDTO);
-        // @Options(useGeneratedKeys = true, keyProperty = "transportationId") 로 PK 자동 주입
-        return outboundVehicleAssignDTO.getTransportationId();
-    }
-
-    @Override
-    public int updatePackingTransportation(int packingId, int transportationId) {
-        return outboundMapper.updatePackingTransportation(packingId, transportationId);
-    }
-
-    @Override
-    public int updateInvoiceNumber(OutboundInvoiceDTO outboundInvoiceDTO) {
-        return outboundMapper.updateInvoiceNumber(outboundInvoiceDTO);
-    }
-
-    @Override
-    public int updateTransportationConfirm(int transportationId, int stateCode) {
-        return outboundMapper.updateTransportationConfirm(transportationId, stateCode);
-    }
-
 }
