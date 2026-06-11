c895e07c6761d3b33c662747dc9f6a761c607f19
mpsddd-commits <mpsddd@gmail.com>
2026-06-11 18:59:25 +0900

    asn 페이지 조회기능 수정 및 추가

--- frontend/src/homes/inbound/Inbound.jsx
+++ frontend/src/homes/inbound/Inbound.jsx
@@ -97,6 +97,34 @@ const Inbound = () => {
     const [size, setSize] = useState(20);
     const [detailData, setDetailData] = useState(null);
 
+
+    const [firstDate, setFirstDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+
+    const getFirstDay = () => {
+        const today = new Date();
+        // 이번 달 1일 구하기 ("YYYY-MM-01")
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const firstDayStr = `${year}-${month}-01`;
+        return firstDayStr;
+    }
+
+    const getLastDayOfMonth = () => {
+        const today = new Date();
+        const year = today.getFullYear();
+        // today.getMonth() + 1 은 '다음 달'의 인덱스가 됩니다.
+        // 일(Day) 자리에 0을 주면 '이번 달의 마지막 날' 객체가 생성됩니다.
+        const lastDay = new Date(year, today.getMonth() + 1, 0);
+
+        const lastYear = lastDay.getFullYear();
+        const lastMonth = String(lastDay.getMonth() + 1).padStart(2, '0');
+        const lastDate = String(lastDay.getDate()).padStart(2, '0');
+
+        const lastDayStr = `${lastYear}-${lastMonth}-${lastDate}`;
+        return lastDayStr; // 예: "2026-06-30" 또는 "2026-02-28" 등 자동 계산
+    };
+
     const openDetailModal = (id) => {
         POST(`/inbound/${id}`).then(res => {
             if (res.status === true) {
