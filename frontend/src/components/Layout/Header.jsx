dad3dbcc30b2f45f1d03bbf28d7183ed5c5e9d85
choiyc05 <choiyc05@gmail.com>
2026-06-18 14:47:08 +0900

    ws 주소 수정

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -20,9 +20,11 @@ const Header = () => {
   ]);
 
   useEffect(() => {
+    url = import.meta.env.VITE_WEBSOCKET_URL || `ws://${window.location.hostname}:8080/hg-websocket`
+    console.log(url)
     // 1. STOMP 클라이언트 생성
     const client = new Client({
-      brokerURL: `ws://${window.location.hostname}:8080/hg-websocket`,
+      brokerURL: url,
       connectHeaders: {},
 
       onConnect: (frame) => {
